2f7bb3c595bd14f71a79085aafa5b8315e4ef886
naraabinnadhikari <narayanabin223@gmail.com>
2024-10-15 13:47:27 +0545

    another method creating Routes  using Outlet and router = createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
+
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import { Routes, Route } from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
+
+
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className='container'>
-      <Routes>
-       <Route path='/' element={<Home />} />
-       <Route path='/products' element={<Products />} />
-       <Route path='/contact' element={<Contact />} />
-       <Route path='/about' element={<About />} />
-      </Routes>
-      </div>
-    </div>
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
