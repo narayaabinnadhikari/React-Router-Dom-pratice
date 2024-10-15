51974ed7f4159303b43e71147fb12b93a5c953f1
naraabinnadhikari <narayanabin223@gmail.com>
2024-10-15 13:32:20 +0545

    Link with to

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,29 @@
 
 import React from 'react'
 import logo from '../assets/Cocacola_logo.png'
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <img src={logo} width={130} alt='Logo' />
-    <ul>
-        <li>Home</li>
-        <li>Our Products</li>
-        <li>Contact Us</li>
-        <li> About Us</li>
-    </ul>
-    <button>Get Started</button>
+    <div className="navbar">
+      <img src={logo} width={130} alt="Logo" />
+      <ul>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/products">
+          <li>Our Products</li>
+        </Link>
+        <Link to="contact">
+          <li>Contact Us</li>
+        </Link>
+        <Link to="about">
+          <li> About Us</li>
+        </Link>
+      </ul>
+      <button>Get Started</button>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
